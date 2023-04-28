25211d215e8ec6c0f9fdaf34876196b2e33df467
Ayat AbuAllan <82124628+ayatallan@users.noreply.github.com>
2023-04-28 20:06:48 +0200

    initialized the navbar component

---
+++ src/components/common/nav.tsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import './common.css'
+import { Navbar, Nav } from "react-bootstrap";
+
+const MyNavbar= () => {
+  return (
+    <Navbar   className="nav-bar">
+      <Navbar.Brand href="#"> ☰</Navbar.Brand >
+      <Navbar.Collapse id="basic-navbar-nav">
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
