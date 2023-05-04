150f194aad9f7c6c8d8a2ab00e60bc3bfa4fb512
dala_bila <dalahhashlamoon@gmail.com>
2023-05-04 12:51:24 +0300

    Create react router and import the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MyNavbar from './components/common/nav';
+import HomePage from './pages/Home/Home.pages';
+import GroupsPage from './pages/Groups/Groups.pages';
+import FormsPage from './pages/Forms/Forms.page';
+import EvaluationPage from './pages/Evaluation/Evaluation.page';
+import StudentEvaluationPage from './pages/Student-Evaluation/StudentEvaluation.page';
 
 function App() {
   return (
     <div className="App">
-      <MyNavbar/>
+      <MyNavbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Home" element={<HomePage />} />
+          <Route path="/Groups" element={<GroupsPage />} />
+          <Route path="/Forms" element={<FormsPage />} />
+          <Route path="/Evaluation" element={<EvaluationPage />} />
+          <Route path="/StudentEvaluation" element={<StudentEvaluationPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
