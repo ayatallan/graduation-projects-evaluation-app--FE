41497d333786fccb2b55b6b7178d5bf6a41f181b
dala_bila <dalahhashlamoon@gmail.com>
2023-05-11 16:50:13 +0300

    enhance the way we hide the navbar  from the sign in page

--- src/components/common/nav-bar/nav.tsx
+++ src/components/common/nav-bar/nav.tsx
@@ -2,14 +2,19 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import './nav.css';
 import { Navbar, Nav } from "react-bootstrap";
-
-const MyNavbar= () => {
+import { useLocation } from "react-router-dom";
+const MyNavbar = () => {
+  const location = useLocation();
+  let isVisible = true;
+  if (location.pathname == '/' || location.pathname == '/SignIn') {
+    isVisible = false;
+  }
   return (
-    <Navbar   className="nav-bar">
+    isVisible? <Navbar className="nav-bar">
       <Navbar.Brand href="#"> ☰</Navbar.Brand >
       <Navbar.Collapse id="basic-navbar-nav">
       </Navbar.Collapse>
-    </Navbar>
+    </Navbar>:<div></div>
   );
 };
 
