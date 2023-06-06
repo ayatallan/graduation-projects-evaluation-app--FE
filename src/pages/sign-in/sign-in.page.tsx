3eadf6b82523551be2108f3bc373ebdaecbbd115
dala_bila <dalahhashlamoon@gmail.com>
2023-06-06 20:10:36 +0300

    navigate to home page

--- src/pages/sign-in/sign-in.page.tsx
+++ src/pages/sign-in/sign-in.page.tsx
@@ -1,13 +1,27 @@
 import { GoogleLogin } from '@react-oauth/google'
 import './sign-in.css'
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 
-const SignInPage = (props : any) => {
-  const responseMessage = (response:any) => {
+const SignInPage = (props: any) => {
+  const [res, setRes] = useState("");
+
+  const navigate = useNavigate();
+  const responseMessage = (response: any) => {
     console.log(response);
-};
-const errorMessage = (error:any) => {
+    setRes(response);
+    console.log("hello");
+
+  };
+
+  useEffect(() => {
+    res && navigate('/Home');
+
+  }, [res])
+
+  const errorMessage = (error: any) => {
     console.log(error);
-};
+  };
 
   return (
     <div className="container1">
@@ -15,8 +29,9 @@ const errorMessage = (error:any) => {
         <img src="logo.png" alt="ppu" />
       </div>
       <div className="SignUpButton">
-            <GoogleLogin onSuccess={responseMessage} onError={() => errorMessage || undefined} />
+        <GoogleLogin onSuccess={responseMessage} onError={() => errorMessage || undefined} />
       </div>
+
     </div>
   )
 }
