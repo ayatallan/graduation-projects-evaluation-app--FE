e8e60eb46cbd5ee3fec7cc4b22f34bbb78cc9407
dala_bila <dalahhashlamoon@gmail.com>
2023-06-06 13:31:17 +0300

    create the sign in  button , and create my own client id

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <GoogleOAuthProvider clientId="1080497894871-78o2eacuebmad7kl0osipcgnfgd4bq30.apps.googleusercontent.com">
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </GoogleOAuthProvider>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/sign-in/sign-in.page.tsx
+++ src/pages/sign-in/sign-in.page.tsx
@@ -1,14 +1,21 @@
+import { GoogleLogin } from '@react-oauth/google'
 import './sign-in.css'
 
 const SignInPage = (props : any) => {
-  
+  const responseMessage = (response:any) => {
+    console.log(response);
+};
+const errorMessage = (error:any) => {
+    console.log(error);
+};
+
   return (
     <div className="container1">
       <div className="ImageSignUpPage">
         <img src="logo.png" alt="ppu" />
       </div>
       <div className="SignUpButton">
-
+            <GoogleLogin onSuccess={responseMessage} onError={() => errorMessage || undefined} />
       </div>
     </div>
   )
