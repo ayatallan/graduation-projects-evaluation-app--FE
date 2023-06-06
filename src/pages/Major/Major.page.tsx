ea510016e2f6ac6e594d371b7d41fbafdb8524fa
yara-sarahna <yarasa2211@gmail.com>
2023-06-06 13:32:35 +0300

    create check boxes for the major select &  css

--- src/pages/Major/Major.page.tsx
+++ src/pages/Major/Major.page.tsx
@@ -1,23 +1,44 @@
 import { Link } from 'react-router-dom';
 import Buttons from '../../components/common/buttons/buttons';
+import { useState } from 'react';
+import './Major.css';
 
-const MajorPage = (props : any) => {
-    return (
-      <div>
-       <p className='path'>{props.path}</p>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-4 col-md-7">
-            <p>Select Majors that the team members belong to  : </p>
-          </div>
-        </div>
-                check boxes 
+const MajorPage = (props: any) => {
+  const [checked, setChecked] = useState<any>([]);
+  const checkList = ["Information Technolgy", "Computer science ", "Comuter Engneering "];
+  const HandleCheck = (e :any)=>{
+    let UpDatedList = [...checked];
+    if(e.target.checked){
+      UpDatedList =[...checked, e.target.value];
 
-      </div>
-
-      </div>
-    )
+    }
+    else{
+      UpDatedList.splice(checked.indexOf(e.target.checked),1)
+    }
+    setChecked(UpDatedList);
   }
+    let isChecked = (item:any) =>
+    checked.includes(item) ? "checked-item" : "not-checked-item";
+    console.log(checked);
+  return (
+   <div>
+    <div className="container">
+        <div className="row"><div className="col-sm-4 col-md-7"> <p>Select Majors that the team members belong to  : </p> </div></div>
+        <div className="list-container">{checkList.map((item, index) => (
+             <div key={index}>
+              <input value ={item} type='checkbox' onChange={HandleCheck}/>
+              <span className={isChecked(item)}>{item}</span>
+            </div>
+
+          ))}
+        </div>
+      </div>
+   </div>
   
-  export default MajorPage ;
   
+  )
+
+}
+  
+
+export default MajorPage;
