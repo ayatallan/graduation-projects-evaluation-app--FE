6de7ba653b6bba15966d3d2aec2bd62ec0ff9a14
Ayat AbuAllan <ayat.online99@hotmail.com>
2023-06-08 13:45:17 +0300

    create evaluation criteria files

---
+++ src/data/rubrics-intro-hardware.ts
@@ -0,0 +1,93 @@
+const RubricsIntroHardware = [
+    {
+      question: "Abstract:",
+      options: [
+        "Contains clear motivation, scope and methodology",
+        "Contains most of them in a clear way",
+        "Contains some of them or not clear enough",
+        "Totally not clear or does not contain any of them"
+      ],
+      answer: 0
+    },
+    {
+      question: "Introduction:",
+      options: [
+        "Contains overview, motivation, scope of the project, objectives and literature review",
+        "Contains most of them in a clear way",
+        "Contains some of them or all but not clear",
+        "Not clear or does not contain any of them"
+      ],
+      answer: 0
+    },
+    {
+      question: "Background:",
+      options: [
+        "Clear definitions and description of all the elements needed",
+        "Unclear description but all elements exist",
+        "Some elements are missing",
+        "Most of the elements are missing"
+      ],
+      answer: 0
+    },
+    {
+      question: "Requirements:",
+      options: [
+        "Problem specific, clearly described, complete and measurable",
+        "Problem specific, not complete, or not clear enough",
+        "Too general requirements or partially not measurable",
+        "Totally not clear"
+      ],
+      answer: 0
+    },
+    {
+      question: "Design Options:",
+      options: [
+        "The report presents a set of arguable design alternatives and justification on the selected one",
+        "The report presents limited arguable design alternatives and justification on the selected one",
+        "The justification is not rational",
+        "There are no design alternatives"
+      ],
+      answer: 0
+    },
+    {
+      question: "General Hardware Design:",
+      options: [
+        "The requirements are completely reflected in the design. Block diagrams or general illustrations",
+        "The requirements are mostly reflected in the design. Diagrams have some mistakes",
+        "Only partially available",
+        "There is a major gap between design and requirements"
+      ],
+      answer: 0
+    },
+    {
+      question: "General Software Design:",
+      options: [
+        "There is enough explanation of the system software design",
+        "Software design is shallowly explained",
+        "Poor software design",
+        "Not present"
+      ],
+      answer: 0
+    },
+    {
+      question: "Language:",
+      options: [
+        "The language in the report is excellent and typo-free",
+        "The language is very good and few typos",
+        "The language is acceptable but contains many typos",
+        "The language is not acceptable"
+      ],
+      answer: 0
+    },
+    {
+      question: "Layout:",
+      options: [
+        "The structure, sectioning and numbering are suitable throughout all the report",
+        "The structure, sectioning and numbering are suitable most of the time",
+        "The structure, sectioning and numbering are not consistent",
+        "They are ill and needs major work"
+      ],
+      answer: 0
+    }
+  ];
+  
