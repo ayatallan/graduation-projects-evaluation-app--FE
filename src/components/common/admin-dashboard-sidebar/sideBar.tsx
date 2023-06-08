d08fcff43e4983a0e1d6eedf161f7c67ea568400
yara-sarahna <yarasa2211@gmail.com>
2023-06-08 13:35:08 +0300

    create admin dashboard sidebar folder

---
+++ src/components/common/admin-dashboard-sidebar/sideBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css'; // Import the CSS file for styling
+
+const SideBar: React.FC = () => {
+  return (
+    <div className="sidebar">
+hbgfhg
+    </div>
+  );
+};
+
+export default SideBar;
