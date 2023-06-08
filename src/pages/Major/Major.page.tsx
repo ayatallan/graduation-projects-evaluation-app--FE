ab641d7f4c6de9e0451e3c3107500b0ba14bb427
yarasarahna <yarasa2211@gmail.com>
2023-06-08 12:04:08 +0300

    fix the splice function problem

--- src/pages/Major/Major.page.tsx
+++ src/pages/Major/Major.page.tsx
@@ -10,24 +10,25 @@ const MajorPage = (props: any) => {
     let UpDatedList = [...checked];
     if(e.target.checked){
       UpDatedList =[...checked, e.target.value];
-
+  
     }
     else{
-      UpDatedList.splice(checked.indexOf(e.target.checked),1)
-    }
+      UpDatedList.splice(UpDatedList.indexOf(e.target.value),1);    
+
+    }      
+    console.log( UpDatedList );
     setChecked(UpDatedList);
   }
     let isChecked = (item:any) =>
     checked.includes(item) ? "checked-item" : "not-checked-item";
     console.log(checked);
+
   return (
    <div>
     <div className="container">
         <div className="row"><div className="col-sm-4 col-md-7"> <p>Select Majors that the team members belong to  : </p> </div></div>
         <div className="list-container">{checkList.map((item, index) => (
              <div key={index}>
-              
-              gggggg
               <input value ={item} type='checkbox' onChange={HandleCheck}/>
               <span className={isChecked(item)}>{item}</span>
             </div>
