56221926069fa6f146046ff19704b4c0eec5abc0
dala_bila <dalahhashlamoon@gmail.com>
2023-06-13 12:04:39 +0300

    start with students page

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import FormsPage from './pages/forms/forms.page';
 import HomePage from './pages/Home/Home.pages';
 import GroupsPage from './pages/Groups/Groups.pages';
 import EvaluationPage from './pages/Evaluation/Evaluation.page';
-import StudentEvaluationPage from './pages/Student-Evaluation/student-evaluation.page';
+
 
 import './App.css';
 import { useEffect, useState } from 'react';
+import Students from './pages/students/studentspage';
+import StudentEvaluationPage from './pages/Student-Evaluation/student-evaluation.page';
 function App() {
   const [initialLocation, setInitialLocation] = useState('');
 
@@ -28,6 +30,7 @@ function App() {
           <Route path="/SignIn" element={<SignInPage />} />
           <Route path="/Groups" element={<GroupsPage path={Path} />} />
           <Route path="/Forms" element={<FormsPage path={Path} />} />
+          <Route path="/students" element={<Students />} />
           <Route path="/Evaluation" element={<EvaluationPage />} />
           <Route path="/StudentEvaluation" element={<StudentEvaluationPage />} />
         </Routes>
