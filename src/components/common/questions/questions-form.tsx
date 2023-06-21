ca7c47a9afa9a69be1d9a6760d51e3d7491da68e
Ayat AbuAllan <82124628+ayatallan@users.noreply.github.com>
2023-06-22 00:49:52 +0300

    convert questions type input to drop-down menu

--- src/components/common/questions/questions-form.tsx
+++ src/components/common/questions/questions-form.tsx
@@ -2,85 +2,87 @@ import React, { useState } from 'react';
 import './form.css';
 import { FormProps } from '../../../interface';
 
-
-
 const QuestionsForm: React.FC<FormProps> = ({ onSubmit }) => {
-    const [question, setQuestion] = useState('');
-    const [options, setOptions] = useState<string[]>(['', '', '', '']);
-    const [type, setType] = useState('');
-    const [weight, setWeight] = useState(0);
-    const [Class, setClass] = useState('');
+  const [question, setQuestion] = useState('');
+  const [options, setOptions] = useState<string[]>(['', '', '', '']);
+  const [type, setType] = useState('');
+  const [weight, setWeight] = useState(0);
+  const [Class, setClass] = useState('');
 
+  const handleQuestionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestion(e.target.value);
+  };
 
-    const handleQuestionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setQuestion(e.target.value);
-    };
+  const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const updatedOptions = [...options];
+    updatedOptions[index] = e.target.value;
+    setOptions(updatedOptions);
+  };
 
-    const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
-        const updatedOptions = [...options];
-        updatedOptions[index] = e.target.value;
-        setOptions(updatedOptions);
-    };
+  const handleWeightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWeight(Number(e.target.value));
+  };
 
-    const handleWeightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setWeight(Number(e.target.value));
-    };
+  const handleClassChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setClass(e.target.value);
+  };
 
+  const handleTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setType(e.target.value);
+  };
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit(question, options, type, Class, weight);
 
-    const handleClassChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setClass(e.target.value);
-    };
+    // Clear the input fields
+    setType('');
+    setWeight(0);
+    setClass('');
+    setQuestion('');
+    setOptions(['', '', '', '']);
+  };
 
-    const handleTypeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setType(e.target.value);
-    };
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <label className="form-label">
+          Question:
+          <input required className="form-input" type="text" value={question} onChange={handleQuestionChange} />
+        </label>
+        <br />
+        {options.map((option, index) => (
+          <label key={index} className="form-label">
+            Option {index + 1}:
+            <input required className="form-input" type="text" value={option} onChange={(e) => handleOptionChange(e, index)} />
+          </label>
+        ))}
+        <br />
+        <label className="form-label">
+          Weight:
+          <input className="form-input" required type="number" value={weight} onChange={handleWeightChange} />
+        </label>
+        <label className="form-label">
+          Class:
+          <input className="form-input" required type="text" value={Class} onChange={handleClassChange} />
+        </label>
+        <label className="form-label">
+          Type:
+          <select className="form-input" required value={type} onChange={handleTypeChange}>
+            <option value="" disabled>
+              Select type
+            </option>
+            <option value="Personal Question">Personal Question</option>
+            <option value="Public">Public</option>
+          </select>
+        </label>
 
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        onSubmit(question, options, type, Class, weight);
-
-        // Clear the input fields
-        setType('');
-        setWeight(0);
-        setClass('');
-        setQuestion('');
-        setOptions(['', '', '', '']);
-    };
-
-    return (
-        <>
-            <form className="form" onSubmit={handleSubmit}>
-                <label className="form-label">
-                    Question:
-                    <input required className="form-input" type="text" value={question} onChange={handleQuestionChange} />
-                </label>
-                <br />
-                {options.map((option, index) => (
-                    <label key={index} className="form-label">
-                        Option {index + 1}:
-                        <input required className="form-input" type="text" value={option} onChange={(e) => handleOptionChange(e, index)} />
-                    </label>
-                ))}
-                <br />
-                <label className="form-label">
-                    weight:
-                    <input className="form-input" required type="number" value={weight} onChange={handleWeightChange} />
-                </label>
-                <label className="form-label">
-                    Class:
-                    <input className="form-input" required type="text" value={Class} onChange={handleClassChange} />
-                </label>
-                <label className="form-label">
-                    Type:
-                    <input className="form-input" required type="text" value={type} onChange={handleTypeChange} />
-                </label>
-
-                <button className="form-button" type="submit">Submit</button>
-            </form>
-        </>
-    );
+        <button className="form-button" type="submit">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default QuestionsForm;
