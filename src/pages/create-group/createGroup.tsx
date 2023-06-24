7bab52de17e973a88c847a8c24c20a3d095554ff
dala_bila <dalahhashlamoon@gmail.com>
2023-06-24 22:13:40 +0300

    import student name to create group

--- src/pages/create-group/createGroup.tsx
+++ src/pages/create-group/createGroup.tsx
@@ -1,38 +1,49 @@
 import { ChangeEvent, useState } from 'react';
 
-import './createGroup'
+import './createGroup.css'
 import Select from '../../components/select/select.component';
-interface Food {
-    foodName: string;
-  }
-
-  interface SelectProps {
+import CheckBox from '../../components/common/toggle-bullets/check-box.component';
+interface Name {
     name: string;
-    label: string;
-    value: number;
-    onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
-  }
+}
+interface StudentName {
+    name: string;
+}
 
 const CreateGroup = () => {
     const [index, setIndex] = useState(0);
-    const dataJson : any = localStorage.getItem('instructors');
-    const data :Food[] = JSON.parse(dataJson);
-    console.log(...data)
-  return (
-    <div>
-       <Select
-                  name="instructor"
-                  label="Instructor"
-                  value={index}
-                  onChange={(e: ChangeEvent<HTMLSelectElement>) => setIndex(Number(e.target.value))}>
-                {data.map((data : any, index : any) =>
-                   <option
-                    key={index}
-                    value={index} >{data.name}
-                  </option>)}
-              </Select>
-    </div>
-  )
+    const dataJson: any = localStorage.getItem('instructors');
+    const data: Name[] = JSON.parse(dataJson);
+
+    const studentsJson: any = localStorage.getItem('students');
+    const students: StudentName[] = JSON.parse(studentsJson);
+
+    return (
+        <div className="create-group">
+            <Select
+                name="instructor"
+                label="Instructor"
+                value={index}
+                onChange={(e: ChangeEvent<HTMLSelectElement>) => setIndex(Number(e.target.value))}>
+                {data.map((data: any, index: any) =>
+                    <option
+                        key={index}
+                        value={index} >{data.name}
+                    </option>)}
+            </Select>
+            <div className="student-name">
+
+                {students.map((data: any, index: any) =>
+                    <CheckBox
+                        key={index}
+                        value={index}
+                        label={data.name}
+                    />
+                )}
+            </div>
+
+        </div>
+    )
 }
 
 export default CreateGroup;
