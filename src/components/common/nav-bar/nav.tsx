2cc1b9a032f69b2a76e378f5cbf97a4d7e8cdb3e
Ayat AbuAllan <82124628+ayatallan@users.noreply.github.com>
2023-06-25 13:24:16 +0300

    Revert "Gpea 7 create admin dashboard sidebar"

--- src/components/common/nav-bar/nav.tsx
+++ src/components/common/nav-bar/nav.tsx
@@ -1,34 +1,17 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import './nav.css';
-import { useState } from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import SideBar from "../admin-dashboard-sidebar/sideBar";
 
 const MyNavbar= () => {
- 
-  const [showSidebar, setShowSidebar] = useState(false);
-
-  const handleToggleSidebar = () => {
-    setShowSidebar(!showSidebar);
-  };
-
   return (
-    <div>
-    <Navbar className="nav-bar">
-      <Navbar.Brand href="#" onClick={handleToggleSidebar}>
-        ☰
-      </Navbar.Brand>
+    <Navbar   className="nav-bar">
+      <Navbar.Brand href="#"> ☰</Navbar.Brand >
       <Navbar.Collapse id="basic-navbar-nav">
-        {/* Render the sidebar if showSidebar is true */}
-     
       </Navbar.Collapse>
-    </Navbar>  
-    {showSidebar && <SideBar />}
-    </div>
+    </Navbar>
   );
 };
-
 
 export default MyNavbar;
                     
