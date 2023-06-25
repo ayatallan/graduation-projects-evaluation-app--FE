ac3febeec2fcfce5ac026def05519322dec5d86c
dala_bilal <dala_bilal@gmail.com>
2023-06-25 13:16:47 +0300

    solve some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MyNavbar from './components/common/nav-bar/nav';
 import SignInPage from './pages/sign-in/sign-in.page';
 import GroupsPage from './pages/Groups/Groups.pages';
 import EvaluationPage from './pages/Evaluation/Evaluation.page';
 import StudentEvaluationPage from './pages/Student-Evaluation/student-evaluation.page';
+import MainDashboard from './pages/AdminDashboard/main-admin-dashbored/mainDashboard.page';
+import StudentsPage from './pages/AdminDashboard/students/student.AdminPage';
 
 import './App.css';
 import { useEffect, useState } from 'react';
-function App() {
+import InstructorsPage from './pages/AdminDashboard/instructors/instructors.AdminPage';
+import GroupsAdminPage from './pages/AdminDashboard/groups/groups.AdminPage';
+import MajorsAdminPage from './pages/AdminDashboard/Majors/Majors.AdminPage';
+import RubricsAdminPage from './pages/AdminDashboard/rubrics/Rubrics.AdminPage';
 
-  // const navigate = useNavigate();
+function App() {
   const [initialLocation, setInitialLocation] = useState('');
 
   useEffect(() => {
@@ -19,45 +23,23 @@ function App() {
   }, []);
   const Path: String = `${initialLocation}`;
 
-  const [questions, setQuestions] = useState<Question[]>(() => {
-    const savedQuestions = localStorage.getItem('questions');
-    return savedQuestions ? JSON.parse(savedQuestions) : [];
-  });
-
-  const handleSubmit = (question: string, options: string[], type: string, Class: string, weight: number) => {
-    const newQuestion: Question = {
-      id: Date.now(),
-      question: question,
-      options: options,
-      type: type,
-      Class: Class,
-      weight: weight,
-    };
-
-    const updatedQuestions = [...questions, newQuestion];
-    setQuestions(updatedQuestions);
-
-    // Save the updated questions to local storage
-    localStorage.setItem('questions', JSON.stringify(updatedQuestions));
-
-    console.log('Updated Questions:', updatedQuestions);
-    // navigate('/Questions');
-
-
-  };
-
   return (
     <div className="App">
    
       <BrowserRouter>
          <MyNavbar />
         <Routes>
-          <Route path="/add-questions" element={<QuestionsForm onSubmit={handleSubmit} />} />
-          <Route path="/" element={<SignInPage />} />
+          <Route path="/SignIn" element={<SignInPage />} />
           <Route path="/Groups" element={<GroupsPage path={Path} />} />
-          <Route path="/Questions" element={<SoftwareReport quizData={questions} path="/Questions" />} />
           <Route path="/Evaluation" element={<EvaluationPage />} />
           <Route path="/StudentEvaluation" element={<StudentEvaluationPage />} />
+          <Route path="/AdminDashboard" element={<MainDashboard/>}>
+            <Route path="StudentsPage" element={<StudentsPage />} />
+            <Route path="InstructorsPage" element={<InstructorsPage/>} />
+            <Route path="GroupsAdminPage" element={<GroupsAdminPage/>} />
+            <Route path="MajorsAdminPage" element={<MajorsAdminPage/>} />
+            <Route path="RubricsAdminPage" element={<RubricsAdminPage/>} />
+          </Route>
         </Routes>
       </BrowserRouter>
 
