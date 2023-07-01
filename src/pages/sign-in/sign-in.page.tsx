4699c75b3cfa83ff94c9eb7c2dfb8482e0504d5e
Ayat AbuAllan <82124628+ayatallan@users.noreply.github.com>
2023-07-01 16:38:57 +0300

    itegrate the sign in page with BE , there is a bug here :)

--- src/pages/sign-in/sign-in.page.tsx
+++ src/pages/sign-in/sign-in.page.tsx
@@ -7,12 +7,40 @@ const SignInPage = (props: any) => {
   const [res, setRes] = useState("");
 
   const navigate = useNavigate();
-  const responseMessage = (response: any) => {
-    console.log(response);
-    setRes(response);
-    console.log("hello");
-
+  const responseMessage = async (response:any) => {
+    try {
+      console.log(response);
+  
+      // Create a user object with the required fields
+      const user = {
+        id: response.id,
+        name: response.name,
+        email: response.email,
+      };
+  
+      // Make a POST request to the backend API
+      const res = await fetch('http://localhost:3002/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+  
+      if (res.ok) {
+        // User created successfully
+        setRes(response);
+        console.log('User created successfully');
+        navigate('/Home');
+      } else {
+        // Error occurred while creating the user
+        console.log('Error occurred while creating the user');
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
+  
 
   useEffect(() => {
     res && navigate('/Home');
