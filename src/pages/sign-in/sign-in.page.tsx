36031eaf01ca389809238d0da20dc67a99133442
Ayat AbuAllan <82124628+ayatallan@users.noreply.github.com>
2023-07-01 16:49:55 +0300

    remove integration

--- src/pages/sign-in/sign-in.page.tsx
+++ src/pages/sign-in/sign-in.page.tsx
@@ -7,40 +7,12 @@ const SignInPage = (props: any) => {
   const [res, setRes] = useState("");
 
   const navigate = useNavigate();
-  const responseMessage = async (response:any) => {
-    try {
-      console.log(response);
-  
-      // Create a user object with the required fields
-      const user = {
-        id: response.id,
-        name: response.name,
-        email: response.email,
-      };
-  
-      // Make a POST request to the backend API
-      const res = await fetch('http://localhost:3002/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-  
-      if (res.ok) {
-        // User created successfully
-        setRes(response);
-        console.log('User created successfully');
-        navigate('/Home');
-      } else {
-        // Error occurred while creating the user
-        console.log('Error occurred while creating the user');
-      }
-    } catch (error) {
-      console.log(error);
-    }
+  const responseMessage = (response: any) => {
+    console.log(response);
+    setRes(response);
+    console.log("hello");
+
   };
-  
 
   useEffect(() => {
     res && navigate('/Home');
