3b65f39e31c3d3173f3095d31e8552528afb99e2
dala_bilal <dala_bilal@gmail.com>
2023-07-05 16:57:23 +0300

    render the group name , students name and instructor for each group

--- src/pages/Report/report.page.tsx
+++ src/pages/Report/report.page.tsx
@@ -1,56 +1,98 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import "./report.css";
-import { StudentReportInfo } from '../../interface';
-const ReportPage = () => {
+import { GroupInfo, Instructor, Student } from '../../interface';
 
-  const students:StudentReportInfo [] = [
-    {
-      id: 1,
-      name: "John Doe",
-      groupName: "Group A",
-      projectName:"project 1",
-      result: 85,
-      instructorName: "Jane Smith",
-    },
-    {
-      id: 2,
-      name: "Alice Johnson",
-      groupName: "Group B",
-      projectName:"project 2",
-      result: 92,
-      instructorName: "Bob Anderson",
-    },
-    // Add more student objects here...
-  ];
-  return (
-    <div className="student-table-container">
-    <table className="student-table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Group Name</th>
-          <th>Project Name</th>
-          <th>Result</th>
-          <th>Instructor Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {students.map((student) => (
-          <tr key={student.id}>
-            <td>{student.id}</td>
-            <td>{student.name}</td>
-            <td>{student.groupName}</td>
-            <td>{student.projectName}</td>
-            <td>{student.result}</td>
-            <td>{student.instructorName}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+const ReportPage = () => {
+  const [groups, setGroups] = useState<GroupInfo[]>([]);
+  const [instructors, setInstructors] = useState<Instructor[]>([]);
+  const [students, setStudents] = useState<Student[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetchGroups();
+    fetchInstructors();
+    fetchStudents();
+  }, []);
+
+  const fetchGroups = async () => {
+    try {
+      const response = await fetch('http://localhost:3002/createGroup');
+      const fetchedGroups = await response.json();
+      setGroups(fetchedGroups);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   
-}
+  
+  const fetchInstructors = async () => {
+    try {
+      const response = await fetch('http://localhost:3002/instructors');
+      if (response.ok) {
+        const data = await response.json();
+        setInstructors(data);
+      } else {
+        setError('Failed to fetch instructors');
+      }
+    } catch (error) {
+      setError('Failed to fetch instructors');
+    }
+  };
+
+  const fetchStudents = async () => {
+    try {
+      const response = await fetch('http://localhost:3002/students');
+      const retrievedStudents = await response.json();
+      setStudents(retrievedStudents);
+    } catch (error) {
+      console.error('Error retrieving students:', error);
+    }
+
+  };
+  const findStudent = (studentId: string) => {
+    const student = students.find((student) => student._id === studentId);
+    if (student) {
+      return student.name;
+    }
+    return '';
+  };
+
+  const findInstructor = (instructorId: string) => {
+    const instructor = instructors.find((instructor) => instructor._id === instructorId);
+    if (instructor) {
+      return instructor.name;
+    }
+    return '';
+  };
+
+  return (
+    <div>
+      {groups.map((group , index) => (
+        <div className="student-table-container" key={index}>
+          <p>Group: {group.groupName}</p>
+          <p>insructor Name : {findInstructor(group.instructor)}</p>
+          <table className="student-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Result</th>
+              </tr>
+            </thead>
+            <tbody>
+              {group.students.map((student : any , index : number) => (
+                <tr key={index}>
+                    <td>{index + 1}</td>
+                  <td>{findStudent(student)}</td>
+                  <td>{student.result}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ReportPage;
